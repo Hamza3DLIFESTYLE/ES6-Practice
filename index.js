3df1fe706d5c7056d24ebcc03975b5b87b65b245
Hama3DLIFESTYLE <muhammadalihamza65@gmail.com>
2024-03-19 14:42:19 +0500

    lec-3

---
+++ index.js
@@ -0,0 +1,13 @@
+//lecture 3 ES6
+//PRIMITIVE DATA TYPES IN JS(NOTE) 
+//1) STRING = "HAMZA"
+//2) INT = 10
+//3) FLOT = 10.1
+//4) BOLEAN = TRUE || FALSE
+
+// TEMPLATE LITERALS
+// way of ES5
+var names = "Muhammad Ali Hamza";
+var age = "27";
+console.log(" My Name is " + names + "My age is " + age)
+    // way of ES5

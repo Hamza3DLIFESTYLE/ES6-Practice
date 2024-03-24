63ff86c8cd29a20fc04afa7390192af376844f5b
Hama3DLIFESTYLE <muhammadalihamza65@gmail.com>
2024-03-24 13:47:29 +0500

    Added ES6 Functions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //lecture 3 ES6
-//PRIMITIVE DATA TYPES IN JS(NOTE) 
+//PRIMITIVE DATA TYPES IN JS(NOTE) + names + 
 //1) STRING = "HAMZA"
 //2) INT = 10
 //3) FLOT = 10.1
@@ -7,7 +7,72 @@
 
 // TEMPLATE LITERALS
 // way of ES5
-var names = "Muhammad Ali Hamza";
-var age = "27";
-console.log(" My Name is " + names + "My age is " + age)
-    // way of ES5
+// var names = "Muhammad Ali Hamza";
+// var age = "27";
+// console.log(" My Name is " "My age is " + age)
+// way of ES5
+// undefine var
+// var age =
+//     console.log(age)
+// undefine var
+
+// way of ES6
+// let names = "Muhammad Ali Hamza";
+// let age = "27";
+// console.log(`My Name is ${names} & my age is ${age}`)
+// way of ES6
+
+// Functions in ES5
+// var sub = (20 - 10);
+
+function sub(a, b) {
+    return a - b;
+}
+console.log(sub);
+// Functions in ES5
+
+// Arrow Functions in ES6
+
+// const add1 = (a, b) => {
+//     return a + b;
+// }
+// let ali = add1(50, 50);
+// console.log(ali);
+// Arrow  Functions in ES6
+// Object in ES5
+// var info = {
+//     names: "Hamza",
+//     age: 27,
+//     address: "lahore",
+// }
+// var name1 = info.names;
+// var age1 = info.age;
+// console.log(name1)
+// var City = {
+//     City1: "Lahore",
+//     City2: "Karachi",
+//     City3: "Pindi",
+// }
+// var a = City.City1;
+// var b = City.City2;
+// var c = City.City3;
+// console.log("My city is " + a + "My 2nd City is " + b);
+// Object in ES5
+
+// object in ES6
+// const bio = {
+//     name1: "ali Hamza",
+//     age1: 27,
+//     address: "lahore punjab",
+// }
+// let { name1, age1, address } = bio;
+// console.log(name1, age1, address)
+
+// const data1 = {
+//     fullname: "Muhammad Ali Hamza",
+//     fathername: "Shahid Ali",
+//     Age: 27,
+// }
+// let { fullname, fathername, Age } = data1;
+// console.logc(`My Name is ${fullname} My Father Name is ${fathername} and age is ${Age}`)
+// object in ES6c
